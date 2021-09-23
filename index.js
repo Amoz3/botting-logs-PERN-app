d95cf3bbcd33e5616bbe8a8384daa7e4868fe153
camalCase <camalCase@protonmail.com>
2021-09-23 15:49:53 +0300

    added dotenv to index js server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
+if (process.env.NODE_ENV === 'development') require('dotenv').config()
 
 const path = require('path');
 const PORT = process.env.PORT || 5000;
